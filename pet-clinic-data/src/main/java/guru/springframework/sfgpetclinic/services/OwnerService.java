6bc4e0f5469eab920b367a41ef37b4b1411a0ed2
Mengran <mengrangao@gwu.edu>
2020-06-02 15:06:31 -0400

    Adding bootstrap for data. Closes #14.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/OwnerService.java
@@ -6,11 +6,4 @@ import guru.springframework.sfgpetclinic.model.Owner;
 
 public interface OwnerService extends CrudService<Owner, Long>{
     Owner findByLastName(String lastName);
-
-    //we can delete these since they are implemented in CrudService already
-//    Owner findById(Long id);
-//
-//    Owner save(Owner owner);
-//
-//    Set<Owner> findAll();
 }
